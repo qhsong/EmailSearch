e681ddf294f47629eb7fbbc4a31734016c97512e
qhsong <sqh@sqh.me>
2014-09-21 14:38:28 +0800

    finish version 0.01

--- bloom.c
+++ bloom.c
@@ -1,4 +1,5 @@
 #include<limits.h>
+#include<string.h>
 
 #include "bloom.h"
 #include "hash.h"
@@ -14,18 +15,28 @@ BF* bloom_create()
 {
 	BF *p = (BF *)malloc(sizeof(BF));
 	int count = BITSIZE / sizeof(int) ;
+	int i;
 	p->size = 0;
 	p->filter = (int *) malloc(BITSIZE);
-	for(int i=0 ; i< count ; i++) {
+	for(i=0 ; i< count ; i++) {
 		p->filter[i] = 0;
 	}
 
-	//init function array
+	/* Init the function point */
+
 	func[0] = RSHash;
 	func[1] = JSHash;
 	func[2] = PJWHash;
 	func[3] = ELFHash;
-	func[4] = 
+	func[4] = BKDRHash;
+	func[5] = SDBMHash;
+	func[6] = DJBHash;
+	func[7] = DEKHash;
+	func[8] = BPHash;
+	func[9] = FNVHash;
+	func[10] = APHash;
+
+	return 0;
 }
 
 int bloom_destroy(BF **bfArray)
@@ -35,12 +46,27 @@ int bloom_destroy(BF **bfArray)
 
 int bloom_add(BF **bfArray,char *str)
 {
-	
+	BF *p = *bfArray;
+	int len = strlen(str);
+	int i;
+	for(i = 0 ; i<FUNCNUM ; i++ ) {
+		int temp = func[i](str,len);
+		SETBIT(p->filter,temp);
+	}
+	return 0;
 }
 
-int bloom_check()
+int bloom_check(BF **bfArray,char *str)
 {
-	return 0;
+	BF *p = *bfArray;
+	int len = strlen(str);
+	int i;
+	for(i= 0 ;i <FUNCNUM ;i++) {
+		int temp = func[i](str,len);
+		int result = GETBIT(p->filter,temp);
+		if(result == 0) return 0;
+	}
+	return 1;
 }
 
 void bloom(FILE *pool,FILE *check,FILE *result) {
@@ -48,6 +74,13 @@ void bloom(FILE *pool,FILE *check,FILE *result) {
 	char line[BUFFERSIZE];
 	while(fgets(line,BUFFERSIZE,pool)) {
 		bloom_add(&b,line);
+	}
+	while(fgets(line,BUFFERSIZE,check)) {
+		if(bloom_check(&b,line)){
+			fprintf(result,"yes\n");
+		}else {
+			fprintf(result,"no\n");
+		}
 	}
 
 }
