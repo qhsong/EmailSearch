4f0463e03b44c162495729437e92a7b371a18631
moodle <moodle@moodle.(none)>
2014-09-29 02:17:19 +0800

    Finish Listtrie.

--- main.c
+++ main.c
@@ -19,12 +19,12 @@ int main(int argc, char **argv)
 	
 	FILE *fpStrpool = fopen("strpool.dat","r");
 	FILE *fpCheckedstr = fopen("checkedemail.dat","r");
-	FILE *fpResult2 = fopen("result_trie.dat","w");
+	FILE *fpResult2 = fopen("result_bloom.dat","w");
 	if(fpStrpool == NULL || fpCheckedstr == NULL){
 		printf("Input file not found!\n");
 		return 0;
 	}
-	/*bloom(fpStrpool,fpCheckedstr,fpResult);*/
-	trie(fpStrpool,fpCheckedstr,fpResult2);
+	bloom(fpStrpool,fpCheckedstr,fpResult2);
+	/*trie(fpStrpool,fpCheckedstr,fpResult2);*/
 	return 0;
 }
